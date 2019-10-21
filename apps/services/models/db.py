8dcc6660774209d13b3a61f1567114a77fcea8ae
jiaxin <edison7500@gmail.com>
2019-10-21 13:47:15 +0800

    add django-models-util

--- apps/services/models/db.py
+++ apps/services/models/db.py
@@ -1,18 +1,26 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from model_utils import Choices
 
 
 class Service(models.Model):
+    Vendor = Choices((0, "aliyun", _("aliyun")), (1, "tencent", _("tencent")))
+
     user_id = models.IntegerField()
-    vendor = models.IntegerField()
+    vendor = models.IntegerField(default=Vendor.aliyun, choices=Vendor)
     service = models.IntegerField()
     app_key = models.CharField(null=False, max_length=255)
     app_secret = models.CharField(null=False, max_length=255)
     title = models.CharField(null=False, max_length=255)
     content = models.TextField(default="", blank=True)
 
-    created_at = models.DateTimeField(default=timezone.now, db_index=True, editable=False)
-    updated_at = models.DateTimeField(default=timezone.now, db_index=True, editable=False)
+    created_at = models.DateTimeField(
+        default=timezone.now, db_index=True, editable=False
+    )
+    updated_at = models.DateTimeField(
+        default=timezone.now, db_index=True, editable=False
+    )
 
     def __str__(self):
         return self.title
--- apps/services/tests.py
+++ apps/services/tests.py
@@ -1,8 +1,10 @@
+import json
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 from faker import Faker
 from pprint import pprint
+from apps.services.models import Service
 
 f = Faker()
 
@@ -13,15 +15,16 @@ class ServiceAPITestCase(APITestCase):
         _url = reverse("api:services:index")
         payload = {
             "user_id": f.pyint(),
-            "vendor": "aliyun",
+            "vendor": Service.Vendor.aliyun,
             "service": "sms",
             "app_key": f.pystr(),
             "app_secret": f.md5(),
             "title": f.name(),
-            "content": {"TemplateName": f.name()}
+            "content_json": {"TemplateName": f.name()}
         }
         res = self.client.post(_url, data=payload, format="json")
-        pprint(res.content)
+        # pprint(res.content)
+        pprint(json.loads(res.content))
         # self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
     def test_get_service_list_view(self):
