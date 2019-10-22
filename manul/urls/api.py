514aa146b6f20a8d72eb8f4581d7e8fd08f02b84
jiaxin <edison7500@gmail.com>
2019-10-22 10:21:52 +0800

    add api docs

--- manul/urls/api.py
+++ manul/urls/api.py
@@ -1,5 +1,19 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Manul API",
+        default_version="v1",
+        # contact=openapi.Contact(email=""),
+        license=openapi.License(name="GNU GENERAL PUBLIC LICENSE"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 app_name = "api"
 
@@ -8,7 +22,20 @@ urlpatterns = [
     path("services/", include("apps.services.urls.api.services")),
 ]
 
-
 urlpatterns = format_suffix_patterns(urlpatterns)
 
-
+urlpatterns += [
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
+]
