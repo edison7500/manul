5de263e73c7b9a2dd3caa0a81bb67f8ff5ebf640
jiaxin <edison7500@gmail.com>
2019-11-06 17:10:54 +0800

    update log config

--- manul/settings/manul_logging.py
+++ manul/settings/manul_logging.py
@@ -1,46 +1,42 @@
+import os
+import environ
+
+ROOT_DIR = environ.Path(__file__) - 3  # three folder back (/a/b/c/ - 3 = /)
+env = environ.Env()
+env.read_env(str(ROOT_DIR.path(".env")))
+
 ##
 # logging
 ##
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
             # 'format': '[%(asctime)s.%(msecs)d] %(levelname)s [%(module)s:%(funcName)s:%(lineno)d]- %(message)s',
         },
-        'error': {
-            'format': '[%(asctime)s.%(msecs)d] [%(module)s:%(funcName)s:%(lineno)d]- %(message)s',
+        "error": {
+            "format": "[%(asctime)s.%(msecs)d] [%(module)s:%(funcName)s:%(lineno)d]- %(message)s"
         },
     },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
+    "handlers": {
+        "null": {"level": "DEBUG", "class": "logging.NullHandler"},
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
         },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        },
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'formatter': 'error',
-            'filename': '/tmp/django.log',
-            'mode': 'a',
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "formatter": "error",
+            "filename": env("DJANGO_LOG_FILE", default="/tmp/django.log"),
+            "mode": "a",
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file', 'console'],
-            'propagate': True,
-            'level': 'INFO',
-        },
-        'django.request': {
-            'handlers': ['file', ],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
+    "loggers": {
+        "django": {"handlers": ["file", "console"], "propagate": True, "level": "INFO"},
+        "django.request": {"handlers": ["file"], "level": "ERROR", "propagate": True},
+    },
 }
