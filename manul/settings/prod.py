a07cbdd503dd0b3203b1936efefccf2f597112b7
jiaxin <edison7500@gmail.com>
2019-11-08 14:50:59 +0800

    update db url

--- manul/settings/prod.py
+++ manul/settings/prod.py
@@ -1,1 +1,10 @@
 from .base import *
+
+
+# DATABASES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    "default": os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
+}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
