5f61e6ac833a3687ba5b401497647c70052201b9
jiaxin <edison7500@gmail.com>
2019-11-08 16:07:39 +0800

    update

--- manul/settings/base.py
+++ manul/settings/base.py
@@ -15,7 +15,7 @@ ALLOWED_HOSTS = ["*"]
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {
-    "default": env.db("DATABASE_URL")
+    "default": env.db("DATABASE_URL", default="sqlite:////tmp/my-tmp-sqlite.db")
 }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
--- manul/settings/prod.py
+++ manul/settings/prod.py
@@ -1,11 +1,1 @@
 from .base import *
-import os
-
-
-# DATABASES
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {
-    "default": env.db("DATABASE_URL")
-}
-DATABASES["default"]["ATOMIC_REQUESTS"] = True
