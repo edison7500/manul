6dc9e1a56533ace5ea8dbf6abfa1746fb63d3577
jiaxin <edison7500@gmail.com>
2019-11-08 15:58:44 +0800

    update

--- manul/settings/prod.py
+++ manul/settings/prod.py
@@ -5,7 +5,6 @@ import os
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-print(env.db("DATABASE_URL"))
 DATABASES = {
     "default": env.db("DATABASE_URL")
 }
